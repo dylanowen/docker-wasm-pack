a3d4d5dc8837ddc96e1f8deddf12ee15b6342d5f
Dylan Owen <dylan.owen@workday.com>
2023-07-23 15:10:11 -0400

    Workflow refactor

---
+++ createTag.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu
+
+VERSION_TAG=$(head -1 Dockerfile | cut -d ':' -f 2)
+
+if git rev-parse "VERSION_TAG" >/dev/null 2>&1; then
+  git tag ${VERSION_TAG} &> /dev/null
+  git push origin --tag
+  echo "true";
+else
+  echo "false"
+fi
--- updateImage.sh
+++ updateImage.sh
@@ -3,8 +3,15 @@ set -eu
 
 BASE_DIR=$(cd $(dirname $0); pwd)
 
-
-
-RUST_VERSION=${1}
+# If this stops working we could maybe use https://forge.rust-lang.org/infra/channel-layout.html
+RUST_VERSION=$(rustc --version | cut -d ' ' -f 2)
 
 sed "s/%%RUST_VERSION%%/${RUST_VERSION}/g" ${BASE_DIR}/Dockerfile.template > ${BASE_DIR}/Dockerfile
+
+if [[ `git status --porcelain` ]]; then
+  git add -A &> /dev/null
+  git ci -m "Updating to Rust ${RUST_VERSION}" &> /dev/null
+  echo "true"
+else
+  echo "false"
+fi

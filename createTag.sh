364a0942b9c4c00913824d604ced3fc6b23ea630
Dylan Owen <dylan.owen@workday.com>
2023-07-23 16:08:13 -0400

    Fixup tagging

--- createTag.sh
+++ createTag.sh
@@ -4,10 +4,10 @@ set -eu
 VERSION_TAG=$(head -1 Dockerfile | cut -d ':' -f 2)
 >&2 echo "Current Version: ${VERSION_TAG}"
 
-if git rev-parse "VERSION_TAG" >/dev/null 2>&1; then
+if git rev-parse "${VERSION_TAG}" >/dev/null 2>&1; then
+  echo "false"
+else
   git tag ${VERSION_TAG} &> /dev/null
   git push origin --tag
-  echo "true";
-else
-  echo "false"
+  echo "true"
 fi

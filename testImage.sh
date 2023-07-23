944da2829e8f269a052671bab173a64e8b03c135
Dylan Owen <dylan.owen@workday.com>
2023-07-23 09:49:15 -0400

    Get dependencies before blocking github.com

--- testImage.sh
+++ testImage.sh
@@ -6,12 +6,18 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 IMAGE_TAG=${1}
 
 TEST_SCRIPT=$(cat <<END
+# Clean for local testing
+cargo clean ;
+
+# Download dependencies
+cargo check ;
+
 # Make sure we don't need extra binaries
 echo "127.0.0.1   github.com" >> /etc/hosts ;
 echo "127.0.0.1   rust-lang.org" >> /etc/hosts ;
 echo "127.0.0.1   static.rust-lang.org" >> /etc/hosts ;
 
-# build our test project
+# Build our test project
 wasm-pack build
 
 END)

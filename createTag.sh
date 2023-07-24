74f94d554a54a8137dc39730e33e470be2f46fde
Dylan Owen <dylan.owen@workday.com>
2023-07-24 09:48:27 -0400

    Pull the latest version from docker rust, instead of rustc

--- createTag.sh
+++ createTag.sh
@@ -5,9 +5,11 @@ VERSION_TAG=$(head -1 Dockerfile | cut -d ':' -f 2)
 >&2 echo "Current Version: ${VERSION_TAG}"
 
 if git rev-parse "${VERSION_TAG}" >/dev/null 2>&1; then
+  >&2 echo "The Tag: ${VERSION_TAG} already exists"
   echo "false"
 else
   git tag ${VERSION_TAG} &> /dev/null
   git push origin --tag
+  >&2 echo "Created Tag: ${VERSION_TAG}"
   echo "true"
 fi
--- updateImage.sh
+++ updateImage.sh
@@ -4,13 +4,16 @@ set -eu
 BASE_DIR=$(cd $(dirname $0); pwd)
 
 # If this stops working we could maybe use https://forge.rust-lang.org/infra/channel-layout.html
-RUST_VERSION=$(rustc --version | cut -d ' ' -f 2)
+RUST_VERSION=$(docker run --rm rust:latest rustc --version 2>/dev/null | cut -d ' ' -f 2)
+>&2 echo "Latest Rust Docker Version: ${RUST_VERSION}"
 
 sed "s/%%RUST_VERSION%%/${RUST_VERSION}/g" ${BASE_DIR}/Dockerfile.template > ${BASE_DIR}/Dockerfile
 
 if [[ `git status --porcelain` ]]; then
   git add -A &> /dev/null
+  >&2 echo "Updating to ${RUST_VERSION}"
   echo "true"
 else
+  >&2 echo "No changes, ${RUST_VERSION} is the latest"
   echo "false"
 fi
